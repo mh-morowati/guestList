f0e3327061cf14c0cee4fdce3bf91e8091f8c9a9
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-10-03 10:36:33 +0330

    /

--- gusselist/src/props/App.tsx
+++ gusselist/src/props/App.tsx
@@ -5,8 +5,12 @@ import UserSearch from '../state/UserSearch';
 function App() {
   return (
     <div>
-      <GusetList/>
-      <UserSearch/>
+      <div className='inline-block w-[400px] border border-black'>
+        <GusetList />
+      </div>
+      <div className='inline-block w-[400px] border border-black'>
+        <UserSearch />
+      </div>
     </div>
   );
 }
